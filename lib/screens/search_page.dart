9997c947d015eceba2e5914f02252c294bb7e23a
Piyush Vaghasiya <piyushvaghasiya2906@gmail.com>
2022-06-02 17:42:24 +0530

    Update 2 June

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -6,62 +6,127 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            height: 35,
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: TextButton(
-              onPressed: () {},
-              child: Row(
-                children: [
-                  Center(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 8.0),
-                      child: Icon(
-                        Icons.search,
-                        color: Colors.grey[600],
-                        size: 20,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Text(
-                        "Search",
-                        style: TextStyle(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              height: 35,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Center(
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 8.0),
+                        child: Icon(
+                          Icons.search,
                           color: Colors.grey[600],
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
+                          size: 20,
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Text(
+                          "Search",
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          SingleChildScrollView(
-            child: Column(
-              children: [],
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    SearchCollection(),
+                    SearchCollection(),
+                    SearchCollection(),
+                    SearchCollection(),
+                    SearchCollection(),
+                    SearchCollection(),
+                    SearchCollection(),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
 
-class SearchItems extends StatelessWidget {
-  const SearchItems({Key? key}) : super(key: key);
+class SearchCollection extends StatelessWidget {
+  const SearchCollection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: SearchImage(),
+                    flex: 1,
+                  ),
+                  Expanded(
+                    child: SearchImage(),
+                    flex: 1,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: SearchImage(),
+                  ),
+                  Expanded(
+                    child: SearchImage(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class SearchImage extends StatelessWidget {
+  const SearchImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage("assets/images/profile.jpg"),
+        ),
+      ),
+    );
   }
 }
